5447c8858daf1dba0f83baaefd31344ed6197783
Jhonatan Montiel <jhonatanmontiel47@gmail.com>
2025-11-11 18:57:26 -0500

    Construimos la respuesta del servidor (HttpResponse) y le añadimos en un try/catch, con la excepción Exception

--- src/ConversionProcess/CurrencyConversion.java
+++ src/ConversionProcess/CurrencyConversion.java
@@ -1,8 +1,10 @@
 package ConversionProcess;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class CurrencyConversion {
 
@@ -11,6 +13,12 @@ public class CurrencyConversion {
         URI direccion = URI.create("https://v6.exchangerate-api.com/v6/bfe0c0fe009597bfd96e127f/pair/" + baseCurrency + "/" + fateCurrency);
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder().uri(direccion).GET().build();
+
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (Exception e) {
+            throw new RuntimeException("Opción no disponible");
+        }
     }
 
 
