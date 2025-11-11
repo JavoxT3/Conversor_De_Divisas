e2f230ed407540ea08626885a2a80bc8cc32b6ea
Jhonatan Montiel <jhonatanmontiel47@gmail.com>
2025-11-11 18:44:47 -0500

    Creamos un método conversión y construimos la solicitud (HttpRequest) para configurar nuestras solicitudes

--- src/ConversionProcess/CurrencyConversion.java
+++ src/ConversionProcess/CurrencyConversion.java
@@ -1,8 +1,17 @@
 package ConversionProcess;
 
+import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
 
 public class CurrencyConversion {
 
-    HttpClient client = HttpClient.newHttpClient();
+    public void conversion (int baseCurrency, int fateCurrency) {
+
+        URI direccion = URI.create("https://v6.exchangerate-api.com/v6/bfe0c0fe009597bfd96e127f/pair/" + baseCurrency + "/" + fateCurrency);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(direccion).GET().build();
+    }
+
+
 }
